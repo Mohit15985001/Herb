5c6c4fe03414b1eb30865c3e03eefd2bb86e83bc
Mohit15985001 <nathwanimohit15@gmail.com>
2024-10-01 09:52:03 +0530

    cors introduced

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const XLSX = require('xlsx');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static(__dirname));
 
 
